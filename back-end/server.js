575cd2c7c47b346352969fe85491b3335f2c2789
AndyFranck23 <ironmanandy23@gmail.com>
2025-01-12 09:51:38 +0300

    essai5

--- back-end/server.js
+++ back-end/server.js
@@ -18,9 +18,8 @@ const NOM_DE_DOMAIN = process.env.VITE_API_URL
 
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173/", // Domaine autorisé
+    origin: "http://localhost:5173", // Domaine autorisé
     credentials: true, // Permettre l'envoi des cookies
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 app.use(bodyParser.json({ limit: '10mb' })); // Par exemple, 50mb
 app.use(cookieParser());
